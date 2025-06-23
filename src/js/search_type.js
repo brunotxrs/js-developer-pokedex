3099bbfdcd00644842d7545a2525a1f55237db9f
brunotxrs <183097017+brunotxrs@users.noreply.github.com>
2025-06-23 11:55:29 -0400

    feat: create the file search_type.js and inside it there is the function to select the type of search

---
+++ src/js/search_type.js
@@ -0,0 +1,31 @@
+function searchType(){
+    //seleção do tipo de busca
+    const buttonSelect = document.getElementById("btn");
+    const boxSelectRadio = document.getElementById("select-radio");
+
+    buttonSelect.addEventListener('click', () => {  
+        boxSelectRadio.classList.remove('hidden');
+    })
+
+    const radioNumber = document.getElementById('number');
+    const radioName = document.getElementById('name');
+
+    radioNumber.addEventListener("click", () =>{
+        buttonSelect.innerHTML = `<span>#</span>`;
+        buttonSelect.style.textDecoration = "none";
+        boxSelectRadio.classList.add('hidden');
+
+    })
+
+    radioName.addEventListener('click', () => {
+        buttonSelect.innerHTML = `<span>A</span>`;
+        buttonSelect.style.textDecoration = "underline";
+        boxSelectRadio.classList.add('hidden');
+        
+    })
+
+    return
+}
+
+
+export { searchType };
